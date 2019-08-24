ed60f383ffbf87528332c5de2ebe3af0335977fc
unknown <simple.ilham@gmail.com>
2019-08-24 10:43:51 +0700

    inisialisasi

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 1;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //memasukan input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //mendeklarasikan vektor move
+        Vector2 move = new Vector2(moveHorizontal, moveVertical);
+
+        //memasukan fungsi gerak
+        Vector2 position = rb.position;
+        position = position + move*speed;
+
+        //eksekusi gerak
+        rb.MovePosition(position);
+
+    }
+}
